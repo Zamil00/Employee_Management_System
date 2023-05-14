fa311ae02132d1a34f8f60a23c802e00678654e6
Hasanov23 <zamilhh@code.edu.az>
2023-05-14 11:44:47 +0200

    Initial commit

---
+++ src/main/java/org/employee/management/system/Conn.java
@@ -0,0 +1,20 @@
+package org.employee.management.system;
+
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "swehasanov");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
